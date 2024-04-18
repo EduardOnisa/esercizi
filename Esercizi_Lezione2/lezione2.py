a0e0e1ca3c7c7413a0d526cc1a8fafc383d4b7ed
EduardOnisa <edysamy04@gmail.com>
2024-04-18 12:26:27 +0200

    First Commit

---
+++ Esercizi_Lezione2/lezione2.py
@@ -0,0 +1,11 @@
+# Eduard Onisa 18/04/2024 
+
+#Use a variable to represent a person's name, and print a message to that person. Your message should be simple, such as, "Hello Eric, would you like to learn some Python today?"
+
+# Questa variabile contiene il nome
+name: str= "Alex"
+
+# Questa variabile contiene il messaggio
+message: str= f"Hello {name}, would you like to learn some Python today?"
+
+print(message)
