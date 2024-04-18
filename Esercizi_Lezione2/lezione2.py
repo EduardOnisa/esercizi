ba6fe654df3fe3c5ebdbc1ca95d05a29d5864c91
EduardOnisa <edysamy04@gmail.com>
2024-04-18 12:52:40 +0200

    risolto secondo esercizio

--- Esercizi_Lezione2/lezione2.py
+++ Esercizi_Lezione2/lezione2.py
@@ -9,3 +9,18 @@ name: str= "Alex"
 message: str= f"Hello {name}, would you like to learn some Python today?"
 
 print(message)
+
+
+
+#Use a variable to represent a person's name, and then print that person's name in lowercase, uppercase, and title case
+
+#Questa è una variabile che contiene il nome di una persona
+name: str= "Alex"
+
+#Questa variabile contiene il nome minuscolo
+name_lower: str = name.lower()
+
+#Questa variabile contiene il nome maiuscolo
+name_upper: str = name.upper()
+
+print (f"{name}, {name_upper}, {name_lower}")
