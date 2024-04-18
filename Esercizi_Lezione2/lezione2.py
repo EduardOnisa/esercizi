8684413487afde2b61fd9ef08bf1ffd6c0da99d7
EduardOnisa <167186885+EduardOnisa@users.noreply.github.com>
2024-04-18 13:55:26 +0200

    Update lezione2.py

--- Esercizi_Lezione2/lezione2.py
+++ Esercizi_Lezione2/lezione2.py
@@ -1,4 +1,4 @@
-# Eduard Onisa 18/04/2024 
+# Eduard Onisa 18/04/2024
 
 #2.3 Use a variable to represent a person's name, and print a message to that person. Your message should be simple, such as, "Hello Eric, would you like to learn some Python today?"
 
